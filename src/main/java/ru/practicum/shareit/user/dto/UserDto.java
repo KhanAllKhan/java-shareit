c799ed1f2cf7654a3790a8caaf56a8e35c78a2c9
Khs <saha20142@yandex.kz>
2025-05-02 15:42:43 +0500

    исправления 7

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -11,6 +11,7 @@ public class UserDto {
     private Long id;
     @NotBlank(message = "Имя не может быть пустым")
     private String name;
+    @NotBlank(message = "Email не может быть пустым ")
     @Email(message = "Email должен быть валидным")
     private String email;
 }
