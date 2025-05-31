92057c85f96a5e3e8a56aa11c6e2c15e5196906a
Khs <saha20142@yandex.kz>
2025-05-31 17:18:55 +0500

    исправления по ревью 9

--- src/main/java/ru/practicum/shareit/booking/dto/BookingInfo.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingInfo.java
@@ -1,13 +1,14 @@
 package ru.practicum.shareit.booking.dto;
 
 import lombok.*;
-// для отправки
+
 @ToString
 @Getter
 @Setter
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class BookingInfo {
     private Long id;
     private Long bookerId;
