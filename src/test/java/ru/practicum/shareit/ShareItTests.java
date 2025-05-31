0e01b43cc5a29744c81951482ebe379ce2082ef1
Khs <saha20142@yandex.kz>
2025-05-31 18:14:54 +0500

    не знаю в чем причина, надеюсь сработает 3, написал тест

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,13 +1,26 @@
 package ru.practicum.shareit;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class ShareItTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+		// Отправляем GET запрос к эндпоинту actuator health и проверяем, что статус ответа 200 (OK)
+		ResponseEntity<String> response = restTemplate.getForEntity("/actuator/health", String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode(), "Application context should load and actuator be healthy");
 	}
-
 }
