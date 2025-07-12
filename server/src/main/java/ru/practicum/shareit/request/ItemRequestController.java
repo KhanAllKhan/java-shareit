12f8caf58d9280963c363c4d2a7053efbd33e715
Khs <saha20142@yandex.kz>
2025-07-12 12:17:46 +0500

    исправил метод ItemRequestController

--- server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -4,7 +4,9 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.practicum.shareit.dto.ItemRequestDto;
 import ru.practicum.shareit.service.ItemRequestService;
 
@@ -27,21 +29,45 @@ public class ItemRequestController {
     @GetMapping
     public List<ItemRequestDto> getOwnRequests(
             @RequestHeader("X-Sharer-User-Id") Long userId) {
-        return requestService.getOwnRequests(userId);
+
+        List<ItemRequestDto> requests = requestService.getOwnRequests(userId);
+        if (requests.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "У пользователя с id=" + userId + " нет запросов"
+            );
+        }
+        return requests;
     }
 
     @GetMapping("/all")
     public List<ItemRequestDto> getOtherUsersRequests(
             @RequestHeader("X-Sharer-User-Id") Long userId,
             @PositiveOrZero @RequestParam(defaultValue = "0") int from,
-            @Positive @RequestParam(defaultValue = "10") int size) {
-        return requestService.getOtherUsersRequests(userId);
+            @Positive     @RequestParam(defaultValue = "10") int size) {
+
+        List<ItemRequestDto> requests = requestService.getOtherUsersRequests(userId);
+        if (requests.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Нет запросов других пользователей"
+            );
+        }
+        return requests;
     }
 
     @GetMapping("/{requestId}")
     public ItemRequestDto getRequestById(
             @RequestHeader("X-Sharer-User-Id") Long userId,
             @PathVariable Long requestId) {
-        return requestService.getRequestById(requestId, userId);
+
+        ItemRequestDto dto = requestService.getRequestById(requestId, userId);
+        if (dto.getResponses().isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "У запроса с id=" + requestId + " нет ответов"
+            );
+        }
+        return dto;
     }
 }
