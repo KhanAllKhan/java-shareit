03976226c13ba6b8f7bef0582e626ef7ed847f8f
Khs <saha20142@yandex.kz>
2025-07-12 12:42:31 +0500

    баг 1

--- server/src/main/java/ru/practicum/shareit/dto/ItemRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/dto/ItemRequestDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -7,13 +8,21 @@ import java.util.List;
 
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@EqualsAndHashCode
 @ToString
 public class ItemRequestDto {
     private Long id;
     private String description;
     private LocalDateTime created;
+
+
     private List<ItemResponseDto> responses;
+
+    @JsonProperty("items")
+    public List<ItemResponseDto> getItems() {
+        return responses;
+    }
 }
